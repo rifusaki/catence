fff1f151abc72cea9e214716212a5eafc4d9f928
rifusaki <martina@rifusaki.com>
2026-08-17 13:58:11 -0500

    chore: prepare 0.2.0-beta.6 release version mappings

--- console/catence_console/release.py
+++ console/catence_console/release.py
@@ -1,4 +1,4 @@
 """Release-coupled compatibility values for the standalone Console."""
 
-CATENCE_RELEASE_VERSION = "0.2.0-beta.5"
+CATENCE_RELEASE_VERSION = "0.2.0-beta.6"
 CATENCE_PROTOCOL_VERSION = 1
